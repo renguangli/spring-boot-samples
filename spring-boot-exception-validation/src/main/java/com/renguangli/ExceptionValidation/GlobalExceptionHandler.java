8adc70d207a6d1c0119518f90ef515abdb66f31b
renguangli <211887977@qq.com>
2018-09-03 21:56:55 +0800

    style:删除无用import

--- spring-boot-exception-validation/src/main/java/com/renguangli/ExceptionValidation/GlobalExceptionHandler.java
+++ spring-boot-exception-validation/src/main/java/com/renguangli/ExceptionValidation/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 
 /*
