e58388fecbef06e8898087edd97913353015198e
renguangli <211887977@qq.com>
2018-10-14 20:48:01 +0800

    feature:spring-boot-thymeleaf

---
+++ spring-boot-thymeleaf/src/main/java/com/renguangli/thymeleaf/ThymeleafController.java
@@ -0,0 +1,35 @@
+package com.renguangli.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ *
+ * Created by renguangli at 2018/10/13 19:35
+ * @since JDK1.8
+ */
+@Controller
+public class ThymeleafController {
+
+    @GetMapping(value = {"", "/", "index"})
+    public String index(Model model, HttpSession session) {
+        // 网站标题
+        model.addAttribute("title", "dali`s Blogs");
+        // 登陆用户
+        User loginUser = new User(2L, "admin", "admin", LocalDateTime.now());
+        session.setAttribute("login_user", loginUser);
+        model.addAttribute("count", 20);
+        // 用户列表
+        List<User> users = new ArrayList<>();
+        users.add(new User(1L, "admin", "admin", LocalDateTime.now()));
+        users.add(new User(2L, "dali", "dali", LocalDateTime.now()));
+        model.addAttribute("users", users);
+        return "index";
+    }
+}
---
+++ spring-boot-thymeleaf/src/main/java/com/renguangli/thymeleaf/User.java
@@ -0,0 +1,71 @@
+package com.renguangli.thymeleaf;
+
+import java.time.LocalDateTime;
+
+/*
+ *
+ * Created by renguangli at 2018/10/13 19:47
+ * @since JDK1.8
+ */
+public class User {
+
+    private Long userId;
+
+    private String username;
+
+    private String password;
+
+    private LocalDateTime createTime;
+
+    private User(){}
+
+    public User(Long userId, String username, String password, LocalDateTime createTime) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.createTime = createTime;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", createTime=" + createTime +
+                '}';
+    }
+
+}
